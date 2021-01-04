52e3ade00c232a61c14463c9260fb787b442c10e
Shivang Kumar <shivang.kumar@deltaschool.in>
2021-01-04 20:13:11 +0530

    createElement use

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React,{Component} from 'react';
+import './App.css';
+
+class App extends Component {
+  render(){
+    return (
+      // <div className="App">
+      //   <h1>
+      //     Hi,I am React App.
+      //   </h1>
+      // </div>
+      React.createElement('div',{className:"App"},
+        React.createElement('h1',null,'Hi, I am React App')
+      )
+    );
+  }
+}
+
+export default App;
