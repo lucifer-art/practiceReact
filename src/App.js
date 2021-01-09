12c48a2603e56e6b61b3cc2d9b89c1dbd7cc3f9a
Shivang Kumar <shivang.kumar@deltaschool.in>
2021-01-09 19:36:23 +0530

    two way binding example

--- src/App.js
+++ src/App.js
@@ -1,54 +1,64 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      persons:[
-        {id:1,name:'Shivang',age:23},
-        {id:2,name:'Shivang1',age:24},
-        {id:3,name:'Shivang2',age:25},
+      persons: [
+        { id: 1, name: 'Shivang', age: 23 },
+        { id: 2, name: 'Shivang1', age: 24 },
+        { id: 3, name: 'Shivang2', age: 25 },
       ]
     }
   }
-  
-  
-    changeNameHandler = (newName) =>{
-      this.setState({
-        persons:[
-          {id:1,name:newName,age:23},
-          {id:2,name:'Shivang1',age:24},
-          {id:3,name:'Shivang2',age:29},
-        ],
-      })
-    }
-    render(){
-      return (
-        <div className="App">
-          <h1>
-            Hi,I am React App.
-          </h1>
-          <button onClick={() => this.changeNameHandler('Shiv!!!!')}>Switch Name</button>
-          <p>This is to check</p>
-          {
-            this.state.persons.map(person=>{
-              if(person.id == 2){
-                return <Person key={person.id} changeNameHandler={this.changeNameHandler.bind(this,'Shiv123')} name={person.name} age={person.age}>My hobbies are playing chess.</Person>
-              } else{
-                return <Person key={person.id} name={person.name} age={person.age}>My hobbies are playing chess.</Person>
-              }
-              
-            })
-          }
-          {/* <Person name="Shivang" age="23">My hobbies are playing chess.</Person> */}
-        </div>
-        // React.createElement('div',{className:"App"},
-        //   React.createElement('h1',null,'Hi, I am React App')
-        )
-    }
+
+
+  changeNameHandler = (newName) => {
+    this.setState({
+      persons: [
+        { id: 1, name: newName, age: 23 },
+        { id: 2, name: 'Shivang1', age: 24 },
+        { id: 3, name: 'Shivang2', age: 29 },
+      ],
+    })
   }
+
+  nameChangedHandler = (event)=>{
+    this.setState({
+      persons: [
+        { id: 1, name: "Shivang Kumar", age: 23 },
+        { id: 2, name: event.target.value, age: 24 },
+        { id: 3, name: 'Shivang2', age: 29 },
+      ],
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>
+          Hi,I am React App.
+          </h1>
+        <button onClick={() => this.changeNameHandler('Shiv!!!!')}>Switch Name</button>
+        <p>This is to check</p>
+        {
+          this.state.persons.map(person => {
+            if (person.id == 2) {
+              return <Person key={person.id} changed={this.nameChangedHandler} changeNameHandler={this.changeNameHandler.bind(this, 'Shiv123')} name={person.name} age={person.age}>My hobbies are playing chess.</Person>
+            } else {
+              return <Person key={person.id} name={person.name} age={person.age}>My hobbies are playing chess.</Person>
+            }
+
+          })
+        }
+        {/* <Person name="Shivang" age="23">My hobbies are playing chess.</Person> */}
+      </div>
+      // React.createElement('div',{className:"App"},
+      //   React.createElement('h1',null,'Hi, I am React App')
+    )
+  }
+}
 
 export default App;
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,8 +2,8 @@ import React from 'react';
 const person = (props) => {
     return (
         <div>
-            <p onClick={props.changeNameHandler}>I am {props.name} and I am {props.age} old</p>
-            <p>{props.children}</p>
+            <p onClick={props.changeNameHandler}>I am {props.name} and I am {props.age} old. {props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name}></input>
         </div>
     )
 }
