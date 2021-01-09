551d46aa34774a69a7fb08f3a130898a94efddc6
Shivang Kumar <shivang.kumar@deltaschool.in>
2021-01-09 19:09:18 +0530

    uses hook useState

--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
-import React,{Component} from 'react';
+import React,{useState} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
-    persons:[
-      {id:1,name:'Shivang',age:23},
-      {id:2,name:'Shivang1',age:24},
-      {id:3,name:'Shivang2',age:25},
-    ]
-  }
-
-  changeNameHandler = () =>{
-    alert("Was Clicked!!!");
-  }
-  render(){
+const App = props => {
+  const [personState, setPersonState]= useState({
+      persons:[
+        {id:1,name:'Shivang',age:23},
+        {id:2,name:'Shivang1',age:24},
+        {id:3,name:'Shivang2',age:25},
+      ],
+      otherValue:"Some other value"
+    })
+    const changeNameHandler = () =>{
+      setPersonState({
+        persons:[
+          {id:1,name:'Shivang Kumar',age:23},
+          {id:2,name:'Shivang1',age:24},
+          {id:3,name:'Shivang2',age:29},
+        ],
+        otherValue:personState.otherValue
+      })
+    }
     return (
       <div className="App">
         <h1>
           Hi,I am React App.
         </h1>
-        <button onClick={this.changeNameHandler}>Switch Name</button>
+        <button onClick={changeNameHandler}>Switch Name</button>
         <p>This is to check</p>
         {
-          this.state.persons.map(person=>{
+          personState.persons.map(person=>{
             return <Person key={person.id} name={person.name} age={person.age}>My hobbies are playing chess.</Person>
           })
         }
@@ -33,6 +39,14 @@ class App extends Component {
       //   React.createElement('h1',null,'Hi, I am React App')
       )
   }
-}
 
 export default App;
+
+// state = {
+//   persons:[
+//     {id:1,name:'Shivang',age:23},
+//     {id:2,name:'Shivang1',age:24},
+//     {id:3,name:'Shivang2',age:25},
+//   ]
+// }
+
