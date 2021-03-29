c578a47415fcffa08a4cb17b47e9925e2d4d83c6
Shivang Kumar <shivang.kumar@deltaschool.in>
2021-03-29 14:38:18 +0530

    added with classes HOC

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,5 +1,6 @@
 import React,{Component,Fragment} from 'react';
 import Aux from './../../../hoc/Aux';
+import WithClass from './../../../hoc/WithClass'
 import './Person.css';
 import Radium from 'radium';
 
@@ -12,10 +13,10 @@ class Person extends Component{
         }
         return (
             // <div className="Person" style={style}>
-            <Fragment className="Person" style={style}>
+            <WithClass classes="Person" style={style}>
                 <p key="i1" onClick={this.props.click}>I am {this.props.name} and I am {this.props.age} old. {this.props.children}</p>
                 <input key="i2" type="text" onChange={this.props.changed} value={this.props.name}></input>
-            </Fragment>
+            </WithClass>
             // </div>
         )
     }
