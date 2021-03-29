06caa1854a3f663ea2880cfe8d7c81bf5385d68b
Shivang Kumar <shivang.kumar@deltaschool.in>
2021-03-29 14:29:23 +0530

    HOC baasics

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React,{Component} from 'react';
+import Aux from './../../../hoc/Aux';
 import './Person.css';
 import Radium from 'radium';
 
@@ -11,10 +12,10 @@ class Person extends Component{
         }
         return (
             // <div className="Person" style={style}>
-            [
-                <p key="i1" onClick={this.props.click}>I am {this.props.name} and I am {this.props.age} old. {this.props.children}</p>,
+            <Aux className="Person" style={style}>
+                <p key="i1" onClick={this.props.click}>I am {this.props.name} and I am {this.props.age} old. {this.props.children}</p>
                 <input key="i2" type="text" onChange={this.props.changed} value={this.props.name}></input>
-            ]
+            </Aux>
             // </div>
         )
     }
