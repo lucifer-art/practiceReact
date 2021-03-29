1ab2c8309db6e5fcb178dc8e8388877f2c234309
Shivang Kumar <shivang.kumar@deltaschool.in>
2021-03-29 14:18:06 +0530

    reactMemo shouldComponentUpdate PureComponent and JSX rerendering

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,38 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Cockpit.css';
 
-const cockpit = (props) => {
-    const style = {
-        backgroundColor: '#000',
-        font:'inherit',
-        border: '1px solid #000',
-        padding:'8px',
-        borderRadius: '4px',
-        color:'#fff',
-        cursor:'pointer',
-        ':hover':{
-          backgroundColor:'lightGreen',
-          color:'#000'
-        }
-      }
-    const classes = [];
-    if(props.persons.length === 2) {
-      classes.push('red');
-    } if(props.persons.length <= 1){
-      classes.push('red');
-      classes.push('bold');
+const Cockpit = (props) => {
+  useEffect(() => {
+    console.log("{Cockpit.js} useEffect");
+    setTimeout(()=>{
+      alert("data saved");
+    },1000)
+  },[props.persons])
+  const style = {
+    backgroundColor: '#000',
+    font: 'inherit',
+    border: '1px solid #000',
+    padding: '8px',
+    borderRadius: '4px',
+    color: '#fff',
+    cursor: 'pointer',
+    ':hover': {
+      backgroundColor: 'lightGreen',
+      color: '#000'
     }
+  }
+  const classes = [];
+  if (props.persons.length === 2) {
+    classes.push('red');
+  } if (props.persons.length <= 1) {
+    classes.push('red');
+    classes.push('bold');
+  }
 
-    return (
-        <div>
-            <h1>
-                {props.title}
-          </h1>
-            <button onClick={() => props.changed('Shiv!!!!')}>Switch Name</button><br /><br />
-            <button style={style} onClick={props.clicked}>Toggle Name</button>
-            <p className={classes.join(' ')}>This is to check</p>
-        </div>
-    )
+  return (
+    <div>
+      <h1>
+        {props.title}
+      </h1>
+      <button onClick={() => props.changed('Shiv!!!!')}>Switch Name</button><br /><br />
+      <button style={style} onClick={props.clicked}>Toggle Name</button>
+      <p className={classes.join(' ')}>This is to check</p>
+    </div>
+  )
 }
 
-export default cockpit;
+export default Cockpit;
--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -10,10 +10,12 @@ class Person extends Component{
             }
         }
         return (
-            <div className="Person" style={style}>
-                <p onClick={this.props.click}>I am {this.props.name} and I am {this.props.age} old. {this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name}></input>
-            </div>
+            // <div className="Person" style={style}>
+            [
+                <p key="i1" onClick={this.props.click}>I am {this.props.name} and I am {this.props.age} old. {this.props.children}</p>,
+                <input key="i2" type="text" onChange={this.props.changed} value={this.props.name}></input>
+            ]
+            // </div>
         )
     }
 }
