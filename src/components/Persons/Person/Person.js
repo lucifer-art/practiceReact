14dc9cb238eebf2a9eb03597f81a47ae5f1bb277
Shivang Kumar <shivang.kumar@deltaschool.in>
2021-03-30 14:27:42 +0530

    using createRef example

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -6,8 +6,13 @@ import './Person.css';
 import Radium from 'radium';
 
 class Person extends Component{
+    constructor(props){
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
     componentDidMount(){
-        this.inputElement.focus();
+        this.inputElementRef.current.focus();
+        // this.inputElement.focus();
     }
     render(){
         const style = {
@@ -19,7 +24,7 @@ class Person extends Component{
             // <div className="Person" style={style}>
             <WithClass className="Person" style={style}>
                 <p key="i1" onClick={this.props.click}>I am {this.props.name} and I am {this.props.age} old. {this.props.children}</p>
-                <input key="i2" ref={(inputEl)=>{this.inputElement = inputEl}} type="text" onChange={this.props.changed} value={this.props.name}></input>
+                <input key="i2" ref={this.inputElementRef} type="text" onChange={this.props.changed} value={this.props.name}></input>
             </WithClass>
             // </div>
         )
